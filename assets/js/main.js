81eba36f48d27e219c71582955ab0b652af9cb8c
Dario-Miccinelli <poduzzo@hotmail.com>
2022-10-18 16:01:02 +0200

    Aggiunto carousel + some random css

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,18 @@ let colorePreferito = prompt ('Dimmi il tuo colore preferito!');
 
 document.getElementById('password').innerHTML +=
 `${nomeUtente}${cognomeUtente}${colorePreferito}99`;
+
+
+
+var words = [ 'Password Insicure SRL.', 'Creiamo password insicure per tutti. ', 'Dal 1999.' , 'Veramente poco sicure.' , 'Non esitare a contattarci se tieni poco ai tuoi dati.'];
+var counter = 0;
+var word = document.getElementById("keep");
+setInterval(update_carousel_words, 900);
+function update_carousel_words() {
+  word.innerHTML = words[counter];
+  counter++;
+
+  if (counter >= words.length) {
+    counter = 0;
+  }
+}
